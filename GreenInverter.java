a4b25e1dccccd953db634267f4c2829fd6bf267b
bruno78 <devbruno78@gmail.com>
2017-05-07 23:30:02 -0400

    Add Green Inverter

--- GreenInverter.java
+++ GreenInverter.java
@@ -1,24 +1,24 @@
 
 /**
- * Write a description of class ImageInverter here.
+ * Write a description of class GreenInverter here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
 import edu.duke.*;
 import java.io.File;
-public class ImageInverter
+
+public class GreenInverter
 {
-    public ImageResource makeInversion(ImageResource inImage){
+        public ImageResource makeAvgGreen(ImageResource inImage){
         ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
         
         for (Pixel pixel: outImage.pixels()){
             Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
-            int average = (inPixel.getRed() + inPixel.getBlue()) / 2;
             
-            pixel.setRed(255-inPixel.getRed());
-            pixel.setGreen(255-inPixel.getGreen());
-            pixel.setBlue(255-inPixel.getBlue());
+            pixel.setRed(0);
+            pixel.setGreen((inPixel.getRed() + inPixel.getBlue()) / 2 );
+            pixel.setBlue(0);
         }
         return outImage;
     }
@@ -27,9 +27,9 @@ public class ImageInverter
         DirectoryResource dr = new DirectoryResource();
         for (File f: dr.selectedFiles()){
             ImageResource inImage = new ImageResource(f);
-            ImageResource negativeImage = makeInversion(inImage);
+            ImageResource negativeImage = makeAvgGreen(inImage);
             String fileName = inImage.getFileName();
-            String newFileName = "negative-" + fileName;
+            String newFileName = "green-" + fileName;
             negativeImage.setFileName(newFileName);
             negativeImage.draw();
             negativeImage.save();
